b3e9fc26043afb33b31fb0bb952f1fa28e1b5a78
syahdan gumilang <syahdangumilang.sg@gmail.com>
2023-05-26 16:09:28 +0700

    bikin deskripsi makanan tapi belum jadi keburu di suruh OB balik karena pintu mau dikunci

--- app/src/main/java/com/example/restoranlambatsaji/makanan.kt
+++ app/src/main/java/com/example/restoranlambatsaji/makanan.kt
@@ -5,24 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.restoranlambatsaji.databinding.FragmentMakananBinding
 
 
 class makanan : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentMakananBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_makanan, container, false)
+        binding = FragmentMakananBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.makanan1.setOnClickListener {
+            this.findNavController()
+                .navigate(R.id.action_makanan_to_deskripsiMakanan)
+        }
     }
 
 
-    }
 }
